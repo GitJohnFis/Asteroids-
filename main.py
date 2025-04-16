6a7b77f0ba3ccec33940492ba1229db1e1aca0b4
GitJohnFis <jfisiiahi69@gmail.com>
2025-04-16 02:22:25 +0000

    update the constant and main modules

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import pygame
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+from constants import *
+
+# this allows us to use the constants or magic #'s
 def main():
     #print startup message
     print("Starting Asteroids!")
-    
+    print(f"Screen Width: {SCREEN_WIDTH}")
+    print(f"Screen Height: {SCREEN_HEIGHT}"
     #add statement to show the game has begun pythonic way
     if __name__ == "__main__":
 

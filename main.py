e69cab8f65f1f0490a00b902d52ced5da5e811ff
GitJohnFis <jfisiiahi69@gmail.com>
2025-04-16 02:28:02 +0000

    add a .gitignore

--- main.py
+++ main.py
@@ -7,9 +7,26 @@ from constants import *
 # this allows us to use the constants or magic #'s
 def main():
     #print startup message
-    print("Starting Asteroids!")
-    print(f"Screen Width: {SCREEN_WIDTH}")
-    print(f"Screen Height: {SCREEN_HEIGHT}"
+    pygame.init()
+    #initialize the pygame library
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    #create a screen object with the specified width and height
+
+while True:
+        #run the game loop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                #check for quit event
+                pygame.quit()
+                #quit the game
+                return
+        screen.fill((0, 0, 0))
+        #fill the screen with black color
+        pygame.display.flip()
+        #update the display
+    # print("Starting Asteroids!")
+    # print(f"Screen Width: {SCREEN_WIDTH}")
+    # print(f"Screen Height: {SCREEN_HEIGHT}"
     #add statement to show the game has begun pythonic way
     if __name__ == "__main__":
 

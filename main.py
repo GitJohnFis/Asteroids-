3a6718c94701a5afcc888e0fe797bf771ed53614
GitJohnFis <jfisiiahi69@gmail.com>
2025-04-16 02:15:32 +0000

    Create virtual enviornment for pygame

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+def main():
+    #print startup message
+    print("Starting Asteroids!")
+    
+    #add statement to show the game has begun pythonic way
+    if __name__ == "__main__":
+
+     main()

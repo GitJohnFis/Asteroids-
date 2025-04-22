ac462cff873f05494a530660b274c0acb26e3682
GitJohnFis <jfisiiahi69@gmail.com>
2025-04-22 01:27:41 +0000

    fix a few syntax errors and indentaion

--- powerups.py
+++ powerups.py
@@ -1,13 +1,31 @@
-class PowerUp:
+import pygame
+import random
+from constants import *
+
+class PowerUp(pygame.sprite.Sprite):
+    containers = None
+
     def __init__(self, x, y, power_type):
-        self.x = x
-        self.y = y
-        self.radius = 15  # smaller than asteroids
-        self.power_type = power_type  # "shield", "speed", etc
-        # Load appropriate sprite based on type
+        pygame.sprite.Sprite.__init__(self)
+        self.add(self.containers)
+        self.position = pygame.Vector2(x, y)
+        self.power_type = power_type
+        # Create a surface for the power-up
+        self.image = pygame.Surface((20, 20), pygame.SRCALPHA)
+        if power_type == "shield":
+            # Draw a blue shield power-up
+            pygame.draw.circle(self.image, "blue", (10, 10), 10)
+        elif power_type == "speed":
+            # Draw a green speed power-up
+            pygame.draw.circle(self.image, "green", (10, 10), 10)
         
-    def update(self):
-        # Movement logic - maybe they float around?
-        
+        self.rect = self.image.get_rect(center=(x, y))
+        self.radius = 10  # For collision detection
+
+    def update(self, dt):
+        # Make the power-up float or pulse for visual effect
+        # You could add movement or animation here
+        pass
+
     def draw(self, screen):
-        # Draw the power-up sprite
+        screen.blit(self.image, self.rect)
--- shoot.py
+++ shoot.py
@@ -4,7 +4,7 @@ import circleshape from CircleShape
 
 class Shot(CircleShape):
     def __init__(self, x, y):
-        super().__init__(x, y,  SHOT_radius)
+        super().__init__(x, y,  SHOT_RADIUS)
 
     def draw(self, screen):
             pygame.draw.circle(screen, "white", self.position, self.radius, 2)

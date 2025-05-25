8fcad421ebad9c1a05fc19bfcc23504395c3498c
GitJohnFis <jfisiiahi69@gmail.com>
2025-05-26 11:30:08 +1200

    fix: indentation errors now put all class methods nested in the players pov

--- player.py
+++ player.py
@@ -8,11 +8,11 @@
         self.rotation = 0
         self.shoot_timer = 0
  
- def draw(self, screen):
-    pygame.draw.polygon(screen, "white", self.triangle(),2)
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(),2)
  
  
- def triangle(self):
+    def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
         a = self.position + forward * self.radius
@@ -20,7 +20,7 @@
         c = self.position - forward * self.radius + right
         return [a, b, c]
 
-def update(self, dt):
+    def update(self, dt):
         keys = pygame.key.get_pressed()
          self.shoot_timer -= dt
 
@@ -35,30 +35,30 @@ def update(self, dt):
             if keys[pygame.K_SPACE]:
                 self.shoot()
 
-def shoot(self):
-# now you should only be able to shoot if the timer is 0
-if self.shoot_timer > 0: 
-      return
-self.shoot_timer = PLAYER_SHOOT_RATE
-shot = Shot(self.position.x, self.position.y)
-shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOT_SPEED
+   def shoot(self):
+     # now you should only be able to shoot if the timer is 0
+       if self.shoot_timer > 0: 
+          return
+       self.shoot_timer = PLAYER_SHOOT_RATE
+       shot = Shot(self.position.x, self.position.y)
+       shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOT_SPEED
 
 
-def rotate(self, dt):
-   self.rotation += dt * SELF_TURN_SPEED * dt
+  def rotate(self, dt):
+      self.rotation += SELF_TURN_SPEED * dt
 
-def move(self, dt):
-   forward = pygame.Vector2(0, 1).rotate(self.rotation)
-   self.position += forward *  PLAYER_SPEED * dt
+  def move(self, dt):
+      forward = pygame.Vector2(0, 1).rotate(self.rotation)
+      self.position += forward * PLAYER_SPEED * self.speed_boost * dt
 
-# Powerups handling methods
-def activate_shield(self):
+   # Powerups handling methods
+  def activate_shield(self):
       self.shield_active = True
       self.shield_timer = PLAYER_SHIELD_TIME #duration of the shield in seconds
 
-def activate_speed_boost(self):
+  def activate_speed_boost(self):
       self.shield_boost = SPEED_BOOST_MULTI # multiplier for speed
       pygame.time.set_timer(pygame, SPEED_BOOST_DURATION * 1000, 1) #set a timer for the speed boost
 
-def deactivate_speed_boost(self):
+  def deactivate_speed_boost(self):
       self.speed_boost = 1.0 #reset the normal speed
